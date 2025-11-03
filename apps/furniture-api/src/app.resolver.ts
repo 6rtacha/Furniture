194eb6bb9ff235e5bf56a47be0804c3e737e1a25
kevin <ahadjon1999bb@gmail.com>
2025-11-03 17:36:00 +0500

    fix: add async to all GraphQL resolvers

--- apps/furniture-api/src/app.resolver.ts
+++ apps/furniture-api/src/app.resolver.ts
@@ -3,7 +3,7 @@ import { Query, Resolver } from '@nestjs/graphql';
 @Resolver()
 export class AppResolver {
 	@Query(() => String)
-	public sayHello(): string {
+	public async sayHello(): Promise<string> {
 		return 'GraphQL ApI Server';
 	}
 }
